0b0405e411df9342ac7797c01a7aa174d05aa07b
mayoralbmmr <a01800463@tec.mx>
2024-11-11 09:42:30 -0600

    11nov

---
+++ ticket master.py
@@ -0,0 +1,137 @@
+print("Entradas Morat")
+
+while True:
+    print("""
+    (1) 27 noviembre (Monterrey)
+    (2) 28 noviembre (Monterrey)
+    (3) 2 diciembre (Zapopan, Jalisco)
+    (4) 3 diciembre (Zapopan, Jalisco)
+    (5) 8 diciembre (Merida)
+    (6) 13 diciembre (Ciudad de México)
+    (7) 14 diciembre (Ciudad de México)
+    """)
+    
+    respuesta = input("Ingrese el número de opción: ")
+    if respuesta == "1":
+        
+        entradasnov_veintisiete = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasnov_veintisiete == 1:
+            print("Continuar")
+        if entradasnov_veintisiete >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+        asientosnov_veintisiete = input("¿En que fila deseas estar?")
+        print("""
+        (1) PIT
+        (2) Grada Oriente
+        (3) General B
+        (4) Grada Poniente
+        (5) Boxes
+        (6) Grada
+        (7) A1
+        (8) A2
+        (9) A3
+        (10) A4
+        (11) A5
+        (12) A6
+        (13) B1
+        (14) B2
+        (15) B3
+        (16) B4
+        (17) B5
+        (18) B6
+        (19) B7S
+
+        """)
+                
+    if respuesta == "2":
+        
+        entradasnov_veintiocho = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasnov_veintiocho == 1:
+            print("Continuar")
+        if entradasnov_veintiocho >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    if respuesta == "3":
+        
+        entradasdic_dos = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_dos == 1:
+            print("Continuar")
+        if entradasdic_dos >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    if respuesta == "4":
+        
+        entradasdic_tres = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_tres == 1:
+            print("Continuar")
+        if entradasdic_tres >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    if respuesta == "5":
+        
+        entradasdic_ocho = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_ocho == 1:
+            print("Continuar")
+        if entradasdic_ocho >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    if respuesta == "6":
+        
+        entradasdic_trece = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_trece == 1:
+            print("Continuar")
+        if entradasdic_trece >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    if respuesta == "7":
+        
+        entradasdic_catorce = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_catorce == 1:
+            print("Continuar")
+        if entradasdic_catorce >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+
+                    
+    
