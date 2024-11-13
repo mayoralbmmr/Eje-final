2e29e748bb0f47636432e21729219461438509cc
mayoralbmmr <a01800463@tec.mx>
2024-11-12 18:28:54 -0600

    nov12

---
+++ exampleticketmaster.py
@@ -0,0 +1,114 @@
+asientosnov_veintisiete = input("¿En que fila deseas estar?")
+        print("""
+            (1) PIT
+            (2) Grada Oriente
+            (3) General B
+            (4) Grada Poniente
+            (5) Boxes
+            (6) Grada
+            (7) A1
+            (8) A2
+            (9) A3
+            (10) A4
+            (11) A5
+            (12) A6
+            (13) B1
+            (14) B2
+            (15) B3
+            (16) B4
+            (17) B5
+            (18) B6
+            (19) B7
+            (20) B8
+            (21) B9
+            (22) C1
+            (23) C2
+            (24) C3
+            (25) C4
+            """)
+        if asientosnov_veintisiete == 1:
+            
+                
+    elif respuesta == "2":
+        
+        entradasnov_veintiocho = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasnov_veintiocho == 1:
+            print("Continuar")
+        if entradasnov_veintiocho >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    elif respuesta == "3":
+        
+        entradasdic_dos = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_dos == 1:
+            print("Continuar")
+        if entradasdic_dos >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    elif respuesta == "4":
+        
+        entradasdic_tres = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_tres == 1:
+            print("Continuar")
+        if entradasdic_tres >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    elif respuesta == "5":
+        
+        entradasdic_ocho = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_ocho == 1:
+            print("Continuar")
+        if entradasdic_ocho >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    elif respuesta == "6":
+        
+        entradasdic_trece = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_trece == 1:
+            print("Continuar")
+        if entradasdic_trece >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
+                
+    elif respuesta == "7":
+        
+        entradasdic_catorce = int(input("¿Cuántas entradas desea comprar? "))
+        if entradasdic_catorce == 1:
+            print("Continuar")
+        if entradasdic_catorce >= 2:
+            asientos = input("¿Desean estar juntos? ")
+            if asientos == "si":
+                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            elif asientos == "no":
+                print("No es necesario la localizacion de los asientos juntos ")
+            else:
+                print("No es valida esa opción")
--- ticket master.py
+++ ticket master.py
@@ -1,6 +1,5 @@
-print("Entradas Morat")
-
-while True:
+def mostrar_menu_principal():
+    print("\n=== ENTRADAS MORAT ===")
     print("""
     (1) 27 noviembre (Monterrey)
     (2) 28 noviembre (Monterrey)
@@ -9,129 +8,94 @@ while True:
     (5) 8 diciembre (Merida)
     (6) 13 diciembre (Ciudad de México)
     (7) 14 diciembre (Ciudad de México)
+    (8) Salir
     """)
     
-    respuesta = input("Ingrese el número de opción: ")
-    if respuesta == "1":
-        
-        entradasnov_veintisiete = int(input("¿Cuántas entradas desea comprar? "))
-        if entradasnov_veintisiete == 1:
-            print("Continuar")
-        if entradasnov_veintisiete >= 2:
-            asientos = input("¿Desean estar juntos? ")
-            if asientos == "si":
-                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
-            elif asientos == "no":
-                print("No es necesario la localizacion de los asientos juntos ")
-            else:
-                print("No es valida esa opción")
-        asientosnov_veintisiete = input("¿En que fila deseas estar?")
-        print("""
-        (1) PIT
-        (2) Grada Oriente
-        (3) General B
-        (4) Grada Poniente
-        (5) Boxes
-        (6) Grada
-        (7) A1
-        (8) A2
-        (9) A3
-        (10) A4
-        (11) A5
-        (12) A6
-        (13) B1
-        (14) B2
-        (15) B3
-        (16) B4
-        (17) B5
-        (18) B6
-        (19) B7S
-
-        """)
-                
-    if respuesta == "2":
-        
-        entradasnov_veintiocho = int(input("¿Cuántas entradas desea comprar? "))
-        if entradasnov_veintiocho == 1:
-            print("Continuar")
-        if entradasnov_veintiocho >= 2:
-            asientos = input("¿Desean estar juntos? ")
-            if asientos == "si":
-                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
-            elif asientos == "no":
-                print("No es necesario la localizacion de los asientos juntos ")
-            else:
-                print("No es valida esa opción")
-                
-    if respuesta == "3":
-        
-        entradasdic_dos = int(input("¿Cuántas entradas desea comprar? "))
-        if entradasdic_dos == 1:
-            print("Continuar")
-        if entradasdic_dos >= 2:
-            asientos = input("¿Desean estar juntos? ")
-            if asientos == "si":
-                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
-            elif asientos == "no":
-                print("No es necesario la localizacion de los asientos juntos ")
-            else:
-                print("No es valida esa opción")
-                
-    if respuesta == "4":
-        
-        entradasdic_tres = int(input("¿Cuántas entradas desea comprar? "))
-        if entradasdic_tres == 1:
-            print("Continuar")
-        if entradasdic_tres >= 2:
-            asientos = input("¿Desean estar juntos? ")
-            if asientos == "si":
-                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
-            elif asientos == "no":
-                print("No es necesario la localizacion de los asientos juntos ")
-            else:
-                print("No es valida esa opción")
-                
-    if respuesta == "5":
-        
-        entradasdic_ocho = int(input("¿Cuántas entradas desea comprar? "))
-        if entradasdic_ocho == 1:
-            print("Continuar")
-        if entradasdic_ocho >= 2:
-            asientos = input("¿Desean estar juntos? ")
-            if asientos == "si":
-                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
-            elif asientos == "no":
-                print("No es necesario la localizacion de los asientos juntos ")
-            else:
-                print("No es valida esa opción")
-                
-    if respuesta == "6":
-        
-        entradasdic_trece = int(input("¿Cuántas entradas desea comprar? "))
-        if entradasdic_trece == 1:
-            print("Continuar")
-        if entradasdic_trece >= 2:
-            asientos = input("¿Desean estar juntos? ")
-            if asientos == "si":
-                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
-            elif asientos == "no":
-                print("No es necesario la localizacion de los asientos juntos ")
-            else:
-                print("No es valida esa opción")
-                
-    if respuesta == "7":
-        
-        entradasdic_catorce = int(input("¿Cuántas entradas desea comprar? "))
-        if entradasdic_catorce == 1:
-            print("Continuar")
-        if entradasdic_catorce >= 2:
-            asientos = input("¿Desean estar juntos? ")
-            if asientos == "si":
-                print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
-            elif asientos == "no":
-                print("No es necesario la localizacion de los asientos juntos ")
-            else:
-                print("No es valida esa opción")
-
-                    
+def mostrar_menu_asientos():
+    print("""
+    === SELECCIÓN ZONA === 
+    (1) PIT
+    (2) Grada Oriente
+    (3) General B
+    (4) Grada Poniente
+    (5) Boxes
+    (6) Grada
+    (7) A1 - A6
+    (8) B1 - B9
+    (9) C1 - C4  
+    """)
     
+def preguntar_cantidad_de_entradas():
+    while True:
+        try:
+            cantidad = int(input("¿Cuántas entradas desea comprar? "))
+            if cantidad > 0:
+                return cantidad
+            else:
+                print("Por favor, ingrese un número mayor a 0")
+        except ValueError:
+            print("Por favor, ingrese un número valido")
+       
+def preguntar_asientos_juntos():
+    while True:
+        respuesta = input("¿Desean estar juntos? (si/no) ").lower()
+        if respuesta == "si":
+            print("Te localizaremos las mejores entradas que esten disponibles según los criterios de búsqueda ")
+            return True
+        elif respuesta == "no":
+            print("No es necesario la localizacion de los asientos juntos ")
+            return False
+        else:
+            print("No es valida esa opción. Por favor responda 'si' o 'no'")
+        
+def procesar_compra(fecha):
+    print(f"\nProsesando compra para el {fecha}")
+    
+    cantidad = preguntar_cantidad_de_entradas()
+    
+    if cantidad >= 2:
+        preguntar_asientos_juntos()
+        
+    mostrar_menu_asientos()
+    
+    while True:
+        try:
+            zona = int(input("Seleccione la zona deseada (1-9): "))
+            if 1 <= zona <= 9:
+                print(f"\nHa seleccionado la zona {zona}")
+                print("\n==== Resumen de compra =====")
+                print(f"Fecha: {fecha}")
+                print(f"Cantidad de asientos: {cantidad}")
+                print(f"Zona seleccionada: {zona}")
+                break
+            else:
+                print("Por favor, seleccione una zona válida (1-9)")
+                
+        except ValueError:
+            print("Por favor, ingrese un número válido")
+
+def main():
+    fechas = {
+        "1": "27 noviembre (Monterrey)",
+        "2": "28 noviembre (Monterrey)",
+        "3": "2 diciembre (Zapopan, Jalisco)",
+        "4": "3 diciembre (Zapopan, Jalisco)",
+        "5": "8 diciembre (Merida)",
+        "6": "13 diciembre (Ciudad de México)",
+        "7": "14 diciembre (Ciudad de México)"
+    }
+                      
+    while True:
+        mostrar_menu_principal()
+        respuesta = input("Ingrese el número de opción (1-8): ")
+        
+        if respuesta == "8":
+            print("¡Gracias por usar nuestro sistema!")
+            break
+        elif respuesta in fechas:
+            procesar_compra(fechas[respuesta])
+        else:
+            print("Opción no válida. Por favor, seleccione una opción del 1 al 8")
+
+if __name__ == "__main__":
+    main()
